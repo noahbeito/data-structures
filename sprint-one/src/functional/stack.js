0d0b424c173080bd8ae8cefeec013ca7f197f755
Noah Beito <noah.beito@gmail.com>
2023-03-29 15:01:04 -0700

    pass tests for the functional pattern stack in functional/stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,47 @@
 var Stack = function() {
   var someInstance = {};
+  var counter = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
+
+
   someInstance.pop = function() {
+    if (counter > 0) {
+      counter--;
+      var popped = storage[counter];
+      delete storage[counter];
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    return counter;
   };
-
+  // console.log('someInstance', someInstance);
   return someInstance;
 };
+
+
+/*
+
+someInstance = {
+  push: function...,
+  pop: function...,
+  size: number of properties
+}
+
+var pop = function() {
+  // remove most recently added value
+  // decrement counter
+  // return the removed value
+}
+
+*/
