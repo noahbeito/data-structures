cbb0ac04a4cba6fe3a06425a82d792bec81f831e
Noah Beito <noah.beito@gmail.com>
2023-03-29 20:59:52 -0700

    pass tests for stack in es6/stack.js

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,44 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    if (this.count > 0) {
+      this.count--;
+      var poppedValue = this.storage[this.count];
+      delete this.storage[this.count];
+      return poppedValue;
+    }
+  }
+
+  size() {
+    return this.count;
+  }
 }
+
+var Instance = new Stack();
+
+
+
+
+/*
+
+Capitalize your function name to indicate to others that it is intended to be run with the keyword new
+
+Use the keyword new when instantiating your class
+
+Use the keyword this in your constructor
+
+Explicitly declare a class method named constructor
+
+Declare all other class methods within the class declaration
+
+*/
