e39e199d2fc76aecda80acccf5870699fb5a52d5
Noah Beito <noah.beito@gmail.com>
2023-03-29 20:33:42 -0700

    pass tests for queue in psuedoclassical/queue.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.frontOfQueue = 0;
+  this.backOfQueue = 0;
+  this.storage = {};
 };
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.backOfQueue] = value;
+  this.backOfQueue++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.backOfQueue > this.frontOfQueue) {
+    var dequeuedValue = this.storage[this.frontOfQueue];
+    delete this.storage[this.frontOfQueue];
+    this.frontOfQueue++;
+    return dequeuedValue;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.backOfQueue - this.frontOfQueue;
+};
+
+var Instance = new Queue();
 
 
