53da8dd652cbcbf51431a21861ecc6ff3cc72214
Noah Beito <noah.beito@gmail.com>
2023-03-29 21:00:32 -0700

    pass tests for queue in es6/queue.js

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.frontOfQueue = 0;
+    this.backOfQueue = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.storage[this.backOfQueue] = value;
+    this.backOfQueue++;
+  }
+
+  dequeue() {
+    if (this.backOfQueue > this.frontOfQueue) {
+      var dequeuedValue = this.storage[this.frontOfQueue];
+      delete this.storage[this.frontOfQueue];
+      this.frontOfQueue++;
+      return dequeuedValue;
+    }
+  }
+
+  size() {
+    return this.backOfQueue - this.frontOfQueue;
+  }
 }
+
+var Instance = new Queue();
